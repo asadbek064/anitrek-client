15655f3ade106ae796145686f3cb1f6d8e1ae80d
asadbek064 <asadbekasadbek@gmail.com>
2022-10-11 23:58:29 -0400

    first test

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const envVars = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL:"https://ajaxillvjdoaympzpwif.supabase.co",
     NEXT_PUBLIC_SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqYXhpbGx2amRvYXltcHpwd2lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5MDg4NTAsImV4cCI6MTk4MDQ4NDg1MH0.S93eQ_DlGl0KrywDA5v28QOal2_Vqg7JpYs09aLaNYo",
-    NEXT_PUBLIC_NODE_SERVER_URL:"http://localhost:3001/",
+    NEXT_PUBLIC_NODE_SERVER_URL:"https://animeonline-server.onrender.com",
     ANIMETTV_SERVER_URL:"http://localhost:3011/",
     NEXT_PUBLIC_SOCKET_SERVER_URL:"",
     NEXT_PUBLIC_PROXY_SERVER_URL:"",

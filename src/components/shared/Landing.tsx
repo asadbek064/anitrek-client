90dfa8455147ef5c50db4be87012493417f2c5e5
Asad <asadbekasadbek@gmail.com>
2023-10-10 12:57:50 -0400

    landing page improvmenets

--- src/components/shared/Landing.tsx
+++ src/components/shared/Landing.tsx
@@ -59,13 +59,13 @@ const Landing = () => {
         <div className="my-16 flex justify-center flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
             <div className="">
               <Link href={"/home"} passHref>
-                  <Button aria-label="go to discovery page" className="[font-size:var(--step--1)] md:[font-size:var(--step-1)] tracking-wide font-semibold px-4 py-3  md:py-6 md:px-8 rounded-sm  outline-sky-700" primary>{t("button-search")}</Button>
+                  <Button aria-label="go to discovery page" className="[font-size:var(--step--1)] md:[font-size:var(--step-0)] tracking-wide font-semibold px-4 py-3  md:py-6 md:px-8 rounded-sm  outline-sky-700" primary>{t("button-search")}</Button>
               </Link>
             </div>
 
             <div className="flex justify-center">
               <Link href={"/register"} passHref>
-                  <Button aria-label="sign up" className="[font-size:var(--step--1)] md:[font-size:var(--step-1)] tracking-wide font-semibold px-4 py-3 md:py-6 md:px-8 rounded-sm " secondary>{t("button-register")}</Button>
+                  <Button aria-label="sign up" className="[font-size:var(--step--1)] md:[font-size:var(--step-0)] tracking-wide font-semibold px-4 py-3 md:py-6 md:px-8 rounded-sm " secondary>{t("button-register")}</Button>
               </Link>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "@/components/shared/Head";
 import Landing from "@/components/shared/Landing";
 import NewestComments from "@/components/shared/NewestComments";
 import NewestReviews from "@/components/shared/NewestReviews";
+import RandomGenreSection from "@/components/shared/RandomGenreSection";
 import Section from "@/components/shared/Section";
 import useDevice from "@/hooks/useDevice";
 import useMedia from "@/hooks/useMedia";
@@ -97,7 +98,13 @@ const Home = () => {
 
             </Section> */}
 
-
+            <div className="px-4 md:px-12 lg:px-20 xl:px-28 2xl:px-36
+                [font-size:var(--step-4)] font-bold pb-12"> Explore the Latest Trending Anime & Manga Titles</div>
+                {[...Array(2)].map((_, index) => (
+                // eslint-disable-next-line react/jsx-key
+                  <RandomGenreSection isMobile={isMobile}/>
+                ))}
+             
             {/* <NewestComments type={MediaType.Anime} /> */}
 
 

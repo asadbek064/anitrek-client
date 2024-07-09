e92233198a2243c0cf79bbbf3752d4210e454752
Asad <contact@asadk.dev>
2024-07-08 20:02:00 -0400

    yandex verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta httpEquiv="Cache-Control" content="max-age=7200" />
-          <meta name="msvalidate.01" content="6A3B19EF46021550CCDDCD5F8864E9D1" />
+          <meta name="yandex-verification" content="3d36b370f67247e1" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="icon" href="/favicon.ico" type="image/ico" />
         </Head>
